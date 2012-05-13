53dde7eeab454f1d3ed2f7874729b3c378524b8c
jbrunton <john_brunton@hotmail.co.uk>
2012-05-13 01:00:38 +0100

    fixed #9 - blogs/compose page broken on submit

--- public/js/app/controllers/blogs_controller.js
+++ public/js/app/controllers/blogs_controller.js
@@ -86,13 +86,14 @@ define([
                 // TODO: parameterize resource constructors to be
                 // constructor(data, extensions).
                 // maybe consider separating extensions and includes in request params?
-                var post = new app.resources.new('blog_post', { blog_id: blog_id});
+                var post = app.resources.new('blog_post', { blog_id: blog_id });
                 
-                formExtender.apply(post, {
-                    submit: 'default',
-                    cancel: function() {
-                        app.nav.to('/blogs/' + blog_id + '/view');
-                    }
+                app.core.extend(post, {
+                    formExtender: {
+                        submit: 'default',
+                        cancel: 'default'
+                    },
+                    blogPostExtensions: { tagsContent: true }
                 });
                 
                 app.tmpl.renderPage('edit-post-tmpl', post);
